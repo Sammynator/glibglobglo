b6bd76a365ccbe74ba1df933a026223d45688635
Sammynator <sgerules@gmail.com>
2025-02-09 23:26:30 +0100

    first functionalities and function layout

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
-const item_1 = document.getElementByID("item_1")
+const items = document.querySelectorAll(".item")
+const statusText = document.querySelectorAll("#statusText")
+const restartBtn = document.querySelectorAll("#restartButton")
+const winConditions = [
+    [0, 1, 2], 
+    [3, 4, 5], 
+    [6, 7, 8], 
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+    [0, 4, 8], 
+    [2, 4, 6]  
+]
+let options = ["", "", "", "", "", "", "", "", ""]
+let currentPlayer = "X"
+let running = "false"
 
-function showX(){
-    item_1.style
+function initGame() {
+    items.forEach(item => addEventListener("click", itemClicked()))
+    restartBtn.addEventListener("click", restartGame())
+    statusText.textContent = `${currentPlayer}'s turn`
 }
+
+function itemClicked() {
+    const itemIndex = this.getAttribute("itemIndex")
+
+    if(options[itemIndex] != "" || !running) {
+        return
+
+    }
+    updateItem(this, itemIndex)
+    checkWinner()
+}
+
+function updateItem(item, index) {
+
+}
+
+function changePlayer() {
+
+}
+
+function checkWinner() {
+
+}
+
+function restartGame() {
+
+}
+
+
