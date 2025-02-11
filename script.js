9444252b34cef6710c1807f5626cf369a16e2521
Sammynator <sgerules@gmail.com>
2025-02-11 10:32:42 +0100

    first capabilities to listen for player moves and store them in array

--- script.js
+++ script.js
@@ -2,12 +2,35 @@ const cells = document.querySelectorAll(".cell")
 const statusText = document.querySelectorAll("#statusText")
 const restartBtn = document.querySelectorAll("#restartButton")
 const winConditions = [
-    [0, 1, 2], 
-    [3, 4, 5], 
-    [6, 7, 8], 
-    [0, 3, 6], 
-    [1, 4, 7], 
-    [2, 5, 8], 
-    [0, 4, 8], 
-    [2, 4, 6]  
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
+
+function initGame() {
+    const cells = document.querySelectorAll(".cell");
+    let playerMoves = [];
+
+    cells.forEach(cell => {
+        cell.addEventListener("click", (event) => {
+            const index = event.target.getAttribute("cellIndex"); 
+            playerMoves.push(index); 
+            console.log(playerMoves); 
+        });
+    });
+}
+
+initGame();
+
+//field is added to array of length 3 at first position
+//opp moves to best option, O is placed
+//opp's move is added to opp move array
+//repeat three times
+//check if any winConditions array matches Xs or Os
+//display winner
+
